0ff58932c9d62f7d7aee6bef5dcde54b5377be26
Navaneeth S <83689431+Navaneeth-007@users.noreply.github.com>
2025-07-02 16:46:05 +0530

    fixed admin login and ui completed

--- admin/login/login.js
+++ admin/login/login.js
@@ -1,20 +1,3 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js';
-import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth-compat.js';
-
-// Your Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDXK-0FKH1hC-LiEASaIWolvxqk-2MnbWI",
-    authDomain: "smartfin-40e30.firebaseapp.com",
-    projectId: "smartfin-40e30",
-    storageBucket: "smartfin-40e30.appspot.com",
-    messagingSenderId: "14976464670",
-    appId: "1:14976464670:web:98756ac774dc67b3613b0f"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
 // DOM Elements
 const adminLoginForm = document.getElementById('adminLoginForm');
 const errorMessage = document.getElementById('error-message');
